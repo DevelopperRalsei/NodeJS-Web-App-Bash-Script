85895712ab7bf490e23133643bb685ad8d599eb8
rizayldrm <79636106+rizayldrm@users.noreply.github.com>
2024-05-30 15:39:20 +0300

    Update nodewebapp.sh

--- nodewebapp.sh
+++ nodewebapp.sh
@@ -153,5 +153,5 @@ echo '<nav class="navbar navbar-expand-lg bg-body-tertiary shadow">
 
 cd ../..
 
-echo 'Project has created without any problems. Write "node index.js" to run or write "nodemon index.js" to run in development mode. | Proje hiç bir sıkıntı çıkmadan oluşturuldu. Çalıştırmak için "node index.js" yaz' 
+echo 'Project has created without any problems. Write "node index.js" to run or write "nodemon index.js" to run in development mode. | Proje hiç bir sıkıntı çıkmadan oluşturuldu. Çalıştırmak için "node index.js" ya da geliştirme modunda çalıştırmak için "nodemon index.js" yaz' 
 
