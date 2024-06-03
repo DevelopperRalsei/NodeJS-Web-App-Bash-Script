515e1120472405f62f77abe804fd470050b9d793
rizayldrm <79636106+rizayldrm@users.noreply.github.com>
2024-06-03 14:39:56 +0300

    Update nodewebapp.sh

--- nodewebapp.sh
+++ nodewebapp.sh
@@ -124,6 +124,7 @@ echo '<!DOCTYPE html>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><%= pageTitle %></title>
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="/lib/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/style.css">
 </head>
